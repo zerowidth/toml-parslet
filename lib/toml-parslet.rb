f3da9ea637e9b7664fb09866cdb35010d06d4090
Nathan Witmer <nwitmer@gmail.com>
2013-02-24 09:32:28 -0700

    Bundle gem toml-parslet

---
+++ lib/toml-parslet.rb
@@ -0,0 +1,7 @@
+require "toml-parslet/version"
+
+module Toml
+  module Parslet
+    # Your code goes here...
+  end
+end
---
+++ lib/toml-parslet/version.rb
@@ -0,0 +1,5 @@
+module Toml
+  module Parslet
+    VERSION = "0.0.1"
+  end
+end
